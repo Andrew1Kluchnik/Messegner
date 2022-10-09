b4ebbdf24542cc322affa950e80356baf614e75d
Andrew <zozo1901@mail.ru>
2022-10-09 12:29:05 +0300

    Initial commit

---
+++ src/main/java/com/messenger/Messegner/MessegnerApplication.java
@@ -0,0 +1,13 @@
+package com.messenger.Messegner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessegnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessegnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messenger/Messegner/MessegnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messenger.Messegner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessegnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
